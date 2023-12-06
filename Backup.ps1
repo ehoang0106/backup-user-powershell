d9ba0aa1a269ce586b74206d7700d72b6bd9e4a2
Khoa Hoang <khoang@awakenservices.net>
2023-12-05 16:19:59 -0800

    Successfully run the script auto copy folder from local to QNAP

--- Backup.ps1
+++ Backup.ps1
@@ -1,36 +1,60 @@
-$qnap_username = Read-Host "Enter username: "
-$qnap_password = Read-Host "Enter password: " -AsSecureString
-$qnap_credentials = New-Object System.Management.Automation.PSCredential ($qnap_username, $qnap_password)
-$qnap_ipaddress = Read-Host "Enter network IP Address: "
-$users_path = "\\$qnap_ipaddress\backup\Awaken\Users"
+Write-Host "----------------"
+Write-Host "Network Drive Credential"
+Write-Host "----------------"
+$qnapUsername = Read-Host "Enter username"
+$qnapPassword = Read-Host "Enter password" -AsSecureString
+$qnapCredentials = New-Object System.Management.Automation.PSCredential ($qnapUsername, $qnapPassword)
+$qnapIpaddress = Read-Host "Enter network drive IP Address"
+$usersPath = "\\$qnapIpaddress\backup\Awaken\Users"
+
+
+$directories = 'Desktop', 'Documents', 'Downloads'
+
 
 #try to access the network folder just entered the IP address here. If success write-host successful else unsucessful
 try {
+    Write-Host "Connecting...Please wait!"
+    Start-Sleep -seconds 2
     #create temporary PSDrive
-    New-PSDrive -Name "TempNetworkDrive" -PSProvider FileSystem -Root $users_path -Credential $qnap_credentials
-    Write-Host "Successful connected to the network drive"
+    New-PSDrive -Name "TempNetworkDrive" -PSProvider FileSystem -Root $usersPath -Credential $qnapCredentials | Out-Null
     Write-Host "----------------------------------------"
-    #Test access
-    Get-ChildItem -path "TempNetworkDrive:\" -Name
+    Write-Host "Successful connected to the network drive $qnapIpaddress"
+    Write-Host "----------------------------------------"
+    #Test access -> temporary map a network drive
+    #Get-ChildItem -path "TempNetworkDrive:\" -Name
 
-    $username_existed = $false
-    while(!($username_existed))
+    #Remove the network drive after done
+    remove-PSDrive -Name "TempNetworkDrive"
+
+
+
+    $usernameExisted = $false
+    while(!($usernameExisted))
     {   
         #create username's folder
-        $username = Read-Host -Prompt "Enter username folder: "
-        if(!(Test-Path -Path $users_path))
+        $folderName = Read-Host -Prompt "Enter folder name"
+        Write-Host "Creating $folderName folder..."
+        Start-Sleep 2
+        foreach($dir in $directories)
+        {
+            Write-Host "Creating $dir folder..."
+            start-sleep -second 1.5
+        }
+
+        if(!(Test-Path -Path "$usersPath\$folderName"))
         {
             
-            New-Item -Path $users_path -Name "$username" -ItemType Directory
+            New-Item -Path $usersPath -Name "$folderName" -ItemType Directory | Out-Null
             
             
             #create 3 folder Desktop, Documents and Downloads
-            $directories = 'Desktop', 'Documents', 'Downloads'
             foreach($dir in $directories)
             {
-                New-Item -path $users_path -Name "$dir" -ItemType Directory
+                New-Item -path "$usersPath\$folderName" -Name "$dir" -ItemType Directory
             }
-            $username_existed = $true
+            $usernameExisted = $true
+
+            write-host "Succesfully created folders!"
         }
         else
         {
@@ -38,9 +62,37 @@ try {
         }
     }
 
+    #copy folder from "C:\Users\<Username>, but only copy 3 main folders which is Desktop, Downloads, and Documents"
+    $folderExisted = $false
+
+    while (!($folderExisted)) {
+        $username = Read-Host "Enter the folder username in C drive"
 
 
-    remove-PSDrive -Name "TempNetworkDrive"
+        if((Test-Path -Path "C:\Users\$username\"))
+        {
+            foreach($dir in $directories)
+            {
+                Copy-Item "C:\Users\$username\$dir\*" -Destination "$usersPath\$folderName\$dir\" -Recurse 
+                Write-Host "Copying $dir..."
+                Start-Sleep -seconds 1.5
+
+            }
+            $folderExisted = $true
+            Write-Host "----------------------------"
+            Write-Host "Successfully backed up data!"
+            Write-Host "----------------------------"
+
+            Write-Host "Press any key to exit..."
+            $null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+            
+        }
+        else {
+            Write-Host "Folder <$username> does not exists. Try again!"
+        }
+    }
+    
+    
 }
 catch {
     write-host "Unsuccessful connecting the network drive. Try again!"
