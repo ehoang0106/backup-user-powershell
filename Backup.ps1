d3d6a382b4f13e37318eea74e1071199b6968bbe
Khoa Hoang <khoang@awakenservices.net>
2023-12-15 09:36:23 -0800

    Release production version

--- Backup.ps1
+++ Backup.ps1
@@ -1,55 +1,77 @@
-Write-Host "-------------------------"
-Write-Host "Network Drive Credential"
-Write-Host "-------------------------"
-$qnapUsername = Read-Host "Enter username"
-$qnapPassword = Read-Host "Enter password" -AsSecureString
-$qnapCredentials = New-Object System.Management.Automation.PSCredential ($qnapUsername, $qnapPassword)
-$qnapIpaddress = Read-Host "Enter network drive IP Address"
-$usersPath = "\\$qnapIpaddress\backup\Awaken\Users"
+# $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+# if (-not ($currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+#     $script = $MyInvocation.MyCommand.Definition
+#     Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$script`"" -Verb RunAs
+    
+# }
 
 
+$qnapPath = "\\11.0.0.53\backup\Awaken\Users"
 $directories = @("Desktop", "Documents", "Downloads")
 
+function Connect-QNAP {
+    param (
+   
+        [string] $qnapPath
+    )
+    $isComplete = $false
+
+    while(!($isComplete))
+    {
+        try {
+            Write-Host "-------------------------"
+            Write-Host "Network Drive Credential"
+            Write-Host "-------------------------"
+            $qnapUsername = Read-Host "Enter username"
+            $qnapPassword = Read-Host "Enter password" -AsSecureString
+            $qnapCredentials = New-Object System.Management.Automation.PSCredential ($qnapUsername, $qnapPassword)
+    
+            Write-Host "Connecting...Please wait!"
+            Start-Sleep -seconds 2
+            #create temporary PSDrive
+            
+            $tempDrive = New-PSDrive -Name "TempNetworkDrive" -PSProvider FileSystem -Root $qnapPath -Credential $qnapCredentials -ErrorAction Stop | Out-Null
+            Write-Host $tempDrive
+
+            Write-Host "---------------------------------------------------------"
+            Write-Host "Successful connected to the network drive"
+            Write-Host "---------------------------------------------------------"
+            
+            
+    
+            $isComplete = $true
+            
+        }
+        
+        catch {
+        write-host "Unsuccessful connecting the network drive. Try again!"
+    
+      }
+    }
+}
+    
 
 function Backup-UserData {
     param (
-        [string]$usersPath,
-        [PSCredential]$qnapCredentials,
-        [string]$qnapIpaddress,
+        [string]$qnapPath,
         [string[]]$directories
     )
     try {
-        Write-Host "Connecting...Please wait!"
-        Start-Sleep -seconds 2
-        #create temporary PSDrive
-        
-        $drive = New-PSDrive -Name "TempNetworkDrive" -PSProvider FileSystem -Root $usersPath -Credential $qnapCredentials -ErrorAction Stop | Out-Null
-        Write-Host $drive
-
-        Write-Host "---------------------------------------------------------"
-        Write-Host "Successful connected to the network drive $qnapIpaddress"
-        Write-Host "---------------------------------------------------------"
-        #Test access -> temporary map a network drive
-        #Get-ChildItem -path "TempNetworkDrive:\" -Name
-    
-        
-    
-    
-    
         $usernameExisted = $false
         while(!($usernameExisted))
         {   
-            #create username's folder
-            $folderName = Read-Host -Prompt "Enter folder name"
-            
-            Write-Host "Checking if $folderName folder existed..."
+            #this will get the exact folder name of the current user is logging
+            $userFolderName = $env:Username
+
+
+            Write-Host "Checking if $userFolderName folder existed..."
             Start-Sleep 1
             
-            if(!(Test-Path -Path "$usersPath\$folderName"))
+            if(!(Test-Path -Path "$qnapPath\$userFolderName"))
             {   
                 Write-Host "`nSuccessfully checked!"
                 Start-Sleep 1
-                Write-Host "`nCreating $folderName folder..."
+                Write-Host "`nCreating $userFolderName folder..."
                 Start-Sleep 1
                 foreach($dir in $directories)
                 {   
@@ -62,128 +84,161 @@ function Backup-UserData {
     
                 write-host "`nSuccesfully created folders!`n"
     
-                New-Item -Path $usersPath -Name "$folderName" -ItemType Directory | Out-Null
+                New-Item -Path $qnapPath -Name "$userFolderName" -ItemType Directory | Out-Null
                 
                 #Out-Null is not showing off any information about creating directories
                 
                 #create 3 folder Desktop, Documents and Downloads
                 foreach($dir in $directories)
                 {
-                    New-Item -path "$usersPath\$folderName" -Name "$dir" -ItemType Directory
+                    New-Item -path "$qnapPath\$userFolderName" -Name "$dir" -ItemType Directory
                 }
                 $usernameExisted = $true
     
                 Write-Host "`n"
-    
             }
             else
             {
-                write-host "Folder existed. Try again!`n"
+                
+                write-host "Folder existed!`n"
+                $userFolderName = Read-Host "Enter different folder name"
+                
             }
         }
     
         
-        $isFolderExisted = $true #flag uf folder existed
+
+        $isFolderExisted = $true #flag if folder existed
     
         while ($isFolderExisted) {
-            #debug 
-            $username = Read-Host "Enter the folder username in C drive"
+            #debug -> This is just for debug to test out
+            #$username = Read-Host "Enter the folder username in C drive"
 
-
-            #get the exact folder for the current logged in user to start the copy process
-            #$username = $env:Username
+            #Turn off this will get the exact folder for the current logged in user to start the copy process
+            $userFolderName = $env:Username
     
-            Write-Host "Checking if $username folder existed..."
+            Write-Host "Checking if $userFolderName folder existed..."
             Start-Sleep 1
     
-            if((Test-Path -Path "C:\Users\$username\"))
+            if((Test-Path -Path "C:\Users\$userFolderName\"))
             {
                 Write-Host "Successfully checked!"
                 Start-Sleep 1
                 foreach($dir in $directories)
                 {
                     Write-Host "Copying $dir...`nPlease wait!..."
-                    Start-Sleep -seconds 1.5
-    
-                    robocopy "C:\Users\$username\$dir\" "$usersPath\$folderName\$dir\" /e /r:0 /w:0 /njs /eta
+                    Start-Sleep -seconds 1.5 
+                    robocopy "C:\Users\$userFolderName\$dir\" "$qnapPath\$userFolderName\$dir\" /e /r:0 /w:0 /njs /eta
                     # /e copies subdirectories and include empty directories
                     # /r retry times default is 1million
                     # /w wait time after the failed to copy
                     # /eta estimate time
                     # /nfl no file will be listed
                     # /ndl no directories will be listed
-    
-                
                 }
     
                 $isFolderExisted = $false #if folder does not exist, stop the loop
 
-
                 Write-Host "----------------------------"
                 Write-Host "Successfully backed up data!"
                 Write-Host "----------------------------"
-        
-                
                 Write-Host "Press any key to continue..."
                 $null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown") #this a press any key to continue
                 
             }
             else {
-                Write-Host "Folder <$username> does not exists. Try again!"
+                Write-Host "Folder <$userFolderName> does not exists. Please re-enter."
+                $userFolderName = Read-Host "Enter folder name"
+
             }
         }
-        
-        
     }
     catch {
         #try to access the network folder just entered the IP address here. If success write-host successful else unsucessful
-        write-host "Unsuccessful connecting the network drive. Try again!"
+        write-host "Unsuccessful backup the data. Try again!"
     }
 }
 
 
-function Open-MenuOption {
+
+function Restore-UserData {
     param (
-        $option
+        [string] $qnapPath,
+        [string[]]$directories
     )
 
-    while ($true) {
+    try {
+        Write-Host "`nStarting restore data..."
+        Start-Sleep 1
+        
+        
+        $userFolderName = Read-Host "Enter name of the folder will be restored on QNAP"
+        Write-Host "Checking if $userFolderName existed"
+        Start-Sleep 1
+
+        if (!(Test-Path -path "$qnapPath\$userFolderName"))
+        {
+            Write-Host "`nSuccessfully checked"
+            Start-Sleep 1
+        }
+
+        #get this username of the user is logging in
+        $destinationFolder = $env:Username
+
+        
+        foreach ($dir in $directories)
+        {
+            Write-Host "Restoring $dir...`nPlease wait!..."
+
+            robocopy "$qnapPath\$userFolderName\$dir" "C:\Users\$destinationFolder\$dir" /e /r:0 /w:0 /njs /eta /copy:dat
+        }
+
+        Write-Host "----------------------------"
+        Write-Host "Successfully restored data!"
+        Write-Host "----------------------------"
+        Write-Host "Press any key to continue..."
+        $null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+    }
+    catch{
+        write-host "Unsuccessful restore the data. Try again!"
+    }
+}
+
+
+
+function Backup-Menu {
+
+    while($true){
         Write-Host "`n----------------------"
         Write-Host "Enter (1) to Backup"
         Write-Host "Enter (2) to Restore"
         Write-Host "Enter (3) to Exit"
         Write-Host "----------------------"
-    
-    
+
         $option = Read-Host "Enter option"
-    
-        if ($option -eq "1") {
-            Backup-UserData -usersPath $usersPath -qnapCredentials $qnapCredentials -qnapIpaddress $qnapIpaddress -directories $directories
-            
+
+
+        if ($option -eq "1"){
+            Backup-UserData -qnapPath $qnapPath -directories $directories
         }
-        elseif ($option -eq "2") {
-            Write-Host "-------------------------------------"
-            Write-Host "This function is under developing...`n Try again later :)`n"
-            Write-Host "-------------------------------------"
-    
+        elseif ($option -eq "2"){
+            Restore-UserData -qnapPath $qnapPath -directories $directories
         }
         elseif ($option -eq "3") {
-            #remove temporary mapped networkdrive
-            remove-PSDrive -Name "TempNetworkDrive" -ErrorAction Ignore
+            Remove-PSDrive -Name "TempNetworkDrive" -ErrorAction Ignore
             break
         }
         else {
-            Write-Host "Invalid. Try again!"
+            Write-Host "Invalid input. Try again!"
         }
-        
     }
     
+
 }
 
 
 
-Open-MenuOption
+Connect-QNAP -qnapPath $qnapPath
 
 
-
-
+Backup-Menu
